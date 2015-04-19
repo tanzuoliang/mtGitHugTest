3bf0c90bd97892d21e484a7e5792637b3e930492
tanzuoliang <ysjwdaypm@163.com>
2015-04-19 12:09:43 +0800

    main code enter

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class ThreadTest
+{
+	public:
+		void init();
+		void threadFun();
+	private:
+		std::thread* thread;
+			
+};
+
+
+void ThreadTest::init()
+{
+	thread = new std::thread(ThreadTest::threadFun,this);
+}
+int main(int argc, char** argv)
+ {
+	std::cout << "Hello world!\n";
+}
